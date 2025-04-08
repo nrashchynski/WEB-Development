79838f4b0ff4b48ac4f39f2e3022d1560c09708c
Nazar <nrashchynski@gmail.com>
2025-04-08 19:44:29 +0300

    Create mainPage.js

---
+++ Lab7/js/mainPage.js
@@ -0,0 +1,8 @@
+function loadMainPage() {
+    const content = document.getElementById("content");
+    content.innerHTML = `
+        <h1>Добро Пожаловать на главную страницу, Друзья</h1>
+        <p>Здесь будет главная информация нашего SPA-приложения о авиакомпании</p>
+        <img src="img/Plane-Transparent-Free-PNG.png" alt="Самолет" width="500">
+    `;
+}
